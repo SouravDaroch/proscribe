aa50db0f29a89ad055cb16739866d0f160a1e043
SouravDaroch <souravdaroch984@gmail.com>
2026-04-21 04:10:54 +0530

    fix: delete validation.js

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://proscribe-backend.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
   withCredentials: true, // MANDATORY: This allows cookies to be sent/received
 });
 
